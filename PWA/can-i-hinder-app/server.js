0c348d243ef5c23c913b4249e84de9f2647e4994
Emily-Breen <c00288668@setu.ie>
2026-02-11 21:52:34 +0000

    added in an express server to host the pwa from the dist folder configured static file server and a fallback to index.html server now runs on process.env.PORT or 8080

---
+++ PWA/can-i-hinder-app/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+const distPath = path.join(__dirname, "dist");
+app.use(express.static(distPath));
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist/index.html")));
+
+app.listen(port, () => {
+  console.log(`PWA server listening on ${port}`);
+});
