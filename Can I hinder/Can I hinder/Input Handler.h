3af69dcf403eb401dca0dacb122d80835a53b730
Emily-Breen <c00288668@setu.ie>
2026-02-16 17:37:02 +0000

    Updated InputHandler to improve on controller inputs and handle mouse navigation

--- Can I hinder/Can I hinder/Input Handler.h
+++ Can I hinder/Can I hinder/Input Handler.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include "MathUtils.h"
 
 class InputHandler {
 
@@ -11,7 +12,14 @@ public:
 	sf::Vector2f getMovement();
 	bool isAttackPressed();
 	bool isActionPressed();
-
+	bool menuUpPressed() const;
+	bool menuDownPressed() const;
+	bool menuSelectPressed() const;
+	void handleEvent(const sf::Event& event, const sf::RenderWindow& window, const sf::View& menuView);
+	bool controllerAttackJustPressed() const;
+	bool menuMouseMoved() const;
+	bool menuMouseClickReleased() const;
+	sf::Vector2f menuMousePosition() const;
 	void setDeadZone(float deadZone);
 
 private:
@@ -19,8 +27,20 @@ private:
 	sf::Vector2f m_movement{ 0.f,0.f };
 	bool m_attackPressed{ false };
 	bool m_actionPressed{ false };
-
+	bool m_menuUpPressed{ false };
+	bool m_menuDownPressed{ false };
+	bool m_menuSelectPressed{ false };
+	bool m_controllerAttackJustPressed{ false };
+	bool m_prevControllerAttack{ false };
+	bool m_prevUp{ false };
+	bool m_prevDown{ false };
+	bool m_prevSelect{ false };
 	float m_deadZone{ 15.f };
+
+	// Menu mouse input
+	bool m_menuMouseMoved{ false };
+	bool m_menuMouseClickReleased{ false };
+	sf::Vector2f m_menuMousePos{ 0.f, 0.f };
 
 	unsigned int m_controllerId{ 0 };
 };
