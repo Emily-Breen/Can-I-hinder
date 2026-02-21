241d55adc24ecb9ee1c9301a2e7262d03eae0898
Emily-Breen <c00288668@setu.ie>
2026-02-21 20:37:13 +0000

    trying to use animation in tiled not really working out as anticipated - might nuke and try a different way

--- Can I hinder/Can I hinder/ASSETS/LEVELS/Dungeon/decor.tsx
+++ Can I hinder/Can I hinder/ASSETS/LEVELS/Dungeon/decor.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="decor" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="decorative.png" width="256" height="256"/>
+ <tile id="180">
+  <animation>
+   <frame tileid="180" duration="100"/>
+   <frame tileid="181" duration="100"/>
+   <frame tileid="182" duration="100"/>
+   <frame tileid="183" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
