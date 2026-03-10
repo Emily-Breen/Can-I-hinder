0886ccd22da556fb2c159e3848c89573abcc1b2d
Emily-Breen <c00288668@setu.ie>
2026-03-10 21:39:48 +0000

    Added in game controller to the API that generates a 6 alphanumerical code to allow for multple games to be ran and have unique codes for spectators to join

---
+++ Can I HinderAPI/Can I HinderAPI/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+//this is to create session ids so if multiple instances of the game is ran then the pwas can connect to the correct instance of that game.
+namespace Can_I_HinderAPI.Controllers
+{
+    [ApiController]
+    [Route("api/game")]
+    public class GameController : ControllerBase
+    {
+        [HttpPost("create-session")]
+        public ActionResult<object> CreateSession()
+        {
+            var sessionId = GenerateCode(6);
+            return Ok(new { sessionId });
+        }
+        
+        private static string GenerateCode(int length)
+        {
+            const string chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+
+            var bytes = RandomNumberGenerator.GetBytes(length);
+            var result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[bytes[i] % chars.Length];
+            }
+
+            return new string(result);
+        }
+    }
+}
